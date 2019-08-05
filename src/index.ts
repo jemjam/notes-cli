8aaefc5553054e3cf958d9633dbd090e6397dddc
Jem Bezooyen <hey@jem.dev>
2019-08-05 09:58:51 -0700

    Format dates, then show the results format
    Some experimentation uncovered that there are more than one datetime
    pickers coded to work with inquirer. Date formatting is going to be
    necessary so having some helpers will make sense. However, the first
    datepicker selected uses a more obsucre library and is less
    maintained, so switch code to this variant instead.
    
    - Move typings into their own director
    - Update the notescli script to use the new field and show formatted
    output.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer'
+import moment from 'moment'
 
 console.log('Welcome to notescli.')
 import { Question, Answers } from 'inquirer'
-import imgdatepicker from 'inquirer-datepicker-prompt'
+import imgdatepicker from 'inquirer-datepicker'
 
-inquirer.registerPrompt('datetime', imgdatepicker as inquirer.prompts.PromptConstructor)
+inquirer.registerPrompt(
+    'datetime',
+    imgdatepicker as inquirer.prompts.PromptConstructor,
+)
 
 interface DateQuestion extends Question {
-    format: any[];
+    format: string[];
     initial: Date;
 }
 
+// An example of a date question in action.
 const myQuestion: DateQuestion = {
     name: 'note',
     message: 'This asks for a specific date:',
     type: 'datetime',
-    format: ['yyyy','-', 'mm', '-', 'dd'],
+    format: ['DD', '-', 'MM', '-', 'YYYY', ' ', 'ddd'],
     initial: new Date(),
 }
 
 inquirer
     .prompt([myQuestion])
-    .then((answers: Answers): void => console.log('We got answers\n', answers))
+    .then((answers: Answers): void =>
+        console.log(
+            'We got answers \n',
+            moment(answers.note).format('dddd MMMM Do[,] YYYY'),
+        ),
+    )
