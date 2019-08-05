56d8427b76aa00b5464108442163418d979f23e9
Jem Bezooyen <hey@jem.dev>
2019-08-04 20:16:34 -0700

    Create an example datepicker prompt
    The datepicker is going to be especially important when we start to look
    for previous or future logfiles. This is also a good time to showcase an
    example prompt.
    
    - Install inquirer/inquirer-datepicker-prompt and types
    - Supplement the types with our own index.d.ts
    - Update the index application:
        - Create a datetime question and show response

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
+import inquirer from 'inquirer'
 
-console.log("This is as easy as it comes.")
+console.log('Welcome to notescli.')
+import { Question, Answers } from 'inquirer'
+import imgdatepicker from 'inquirer-datepicker-prompt'
 
+inquirer.registerPrompt('datetime', imgdatepicker as inquirer.prompts.PromptConstructor)
+
+interface DateQuestion extends Question {
+    format: any[];
+    initial: Date;
+}
+
+const myQuestion: DateQuestion = {
+    name: 'note',
+    message: 'This asks for a specific date:',
+    type: 'datetime',
+    format: ['yyyy','-', 'mm', '-', 'dd'],
+    initial: new Date(),
+}
+
+inquirer
+    .prompt([myQuestion])
+    .then((answers: Answers): void => console.log('We got answers\n', answers))
