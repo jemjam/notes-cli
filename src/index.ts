ce16d0ca5216ad74a9e367f0418fc8f37b9f72e3
Jem Bezooyen <hey@jem.dev>
2019-08-11 18:25:09 -0700

    Create structure for multiple commands
    Minimist gives a simple api where we can easilly pull in arguments,
    so a switch statement is all we need to trigger different paths.
    
    - Add start of dailies module, which includes some helpers for mapping
      dates to strings like (today/yesterday/tomorrow)
    - Create switch statement in base IIFE that runs different commands
    - (also commented out some of the config loading, since it's not
      currently relevant; will add again when needed)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer'
 import moment from 'moment'
-import { Answers } from 'inquirer'
-import { dateQuestion } from './datepicker'
-import { loadConfig, NotesConfig } from './config'
+import process from 'process'
+// import { Answers } from 'inquirer'
+// import { dateQuestion } from './datepicker'
+// import { loadConfig, NotesConfig } from './config'
+import { ParsedArgs } from 'minimist'
+import minimist = require('minimist');
+import { dateValues, formattedDateValues } from './dailies';
 
-// Main:
-(async ():Promise<void> => {
+// Main function, immediately executed
+(async (): Promise<void> => {
     console.log('Holaa')
 
-    const config:NotesConfig = await loadConfig()
-    console.log('config async style', config)
+    const cliArgs: ParsedArgs = minimist(process.argv.slice(2))
+    const config: NotesConfig = await loadConfig()
 
-    const answers:Answers = await inquirer.prompt([dateQuestion])
-    console.log('and some answers', answers)
+    const commandToRun = cliArgs._.length > 0 ? cliArgs._[0] : 'noCommand'
+    switch (commandToRun) {
+        case 'open': {
+            console.log('open the file', formattedDateValues)
+            break
+        }
+        case 'create': {
+            console.log('create something great')
+            break
+        }
+        case 'noCommand': {
+            console.log('No command was given')
+            break
+        }
+        default: {
+            console.log("Your command wasn't valid")
+            break
+        }
+    }
+    console.log('arguments', cliArgs)
 
+    // const answers:Answers = await inquirer.prompt([dateQuestion])
+    // console.log('and some answers', answers)
 })()
